23b7445efaeb718e320ae5a65c49bd4a4c4b9199
Clive <clive.binu2@gmail.com>
2025-03-08 22:15:28 -0500

    Configured preloader

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,10 +6,6 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          // "Software Developer",
-          // "Freelancer",
-          // "MERN Stack Developer",
-          // "Open Source Contributor",
           "Astrophysicist",
           "Deep Learning Developer",
         ],
